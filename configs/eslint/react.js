b5c8e6124c5afebd36031d360c5777966ed03c44
Dhairya Rathod <dhairya.rathod24@gmail.com>
2025-03-12 16:26:42 +0530

    converted to Flat Config

--- configs/eslint/react.js
+++ configs/eslint/react.js
@@ -1,25 +1,29 @@
 const reactPlugin = require("eslint-plugin-react");
 const reactHooksPlugin = require("eslint-plugin-react-hooks");
 const jsxA11yPlugin = require("eslint-plugin-jsx-a11y");
+const typescriptConfig = require("./typescript.js");
 
-module.exports = {
-  extends: ["./typescript.js"],
-  plugins: {
-    react: reactPlugin,
-    "react-hooks": reactHooksPlugin,
-    "jsx-a11y": jsxA11yPlugin,
-  },
-  settings: {
-    react: {
-      version: "detect",
+module.exports = [
+  ...typescriptConfig,
+
+  {
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "jsx-a11y": jsxA11yPlugin,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      ...jsxA11yPlugin.configs.recommended.rules,
     },
   },
-  rules: {
-    ...reactPlugin.configs.recommended.rules,
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    ...jsxA11yPlugin.configs.recommended.rules,
-  },
-};
+];
